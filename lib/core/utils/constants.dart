713efc696366e16607ecff9ed34278fe20663ef3
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 18:10:40 +0530

    Added popularMoviesEndpoint to constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,4 +2,5 @@ class Constants {
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   static const String logoBaseUrl = 'https://image.tmdb.org/t/p/original';
+  static const String popularMoviesEndpoint = '/movie/popular';
 }
