3c7f258a34a51cd7f9edf3ccc48102862a986153
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 20:55:21 +0530

    Implemented `main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'core/network/api_client.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'data/repositories/movie_repository.dart';
+import 'data/services/movie_service.dart';
+import 'lib/presentation/viewmodels/movie_view_model.dart';
+import 'lib/presentation/views/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,61 +17,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FlixWatch',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: MultiProvider(providers: [
+        Provider(create: (_) => ApiClient()),
+        ProxyProvider<ApiClient, MovieService>(
+          update: (_, apiClient, __) => MovieService(apiClient),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        ProxyProvider<MovieService, MovieRepository>(
+          update: (_, movieService, __) => MovieRepository(movieService),
+        ),
+        ChangeNotifierProxyProvider<MovieRepository, MovieViewModel>(
+          create: (context) => MovieViewModel(context.read<MovieRepository>()),
+          update: (_, repository, viewModel) =>
+              viewModel ?? MovieViewModel(repository),
+        ),
+      ], child: const HomeScreen()),
     );
   }
 }
