bb34323e1390d2fe72c2a47650a46e11fadf2527
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 23:08:21 +0530

    feat: added `getMovieDetails` feature

--- lib/data/repositories/movie_repository.dart
+++ lib/data/repositories/movie_repository.dart
@@ -1,3 +1,4 @@
+import '../models/movie_details_model.dart';
 import '../models/movie_model.dart';
 import '../services/movie_service.dart';
 
@@ -9,6 +10,14 @@ class MovieRepository {
   Future<List<MovieModel>> getPopularMovies() async {
     try {
       return await _movieService.getPopularMovies();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<MovieDetailsModel> getMovieDetails(String movieID) async {
+    try {
+      return await _movieService.getMovieDetails(movieID);
     } catch (e) {
       rethrow;
     }
--- lib/data/services/movie_service.dart
+++ lib/data/services/movie_service.dart
@@ -1,4 +1,6 @@
+import 'package:flixwatch/core/error/failures.dart';
 import 'package:flixwatch/core/utils/constants.dart';
+import 'package:flixwatch/data/models/movie_details_model.dart';
 
 import '../../core/network/api_client.dart';
 import '../models/movie_model.dart';
@@ -14,7 +16,17 @@ class MovieService {
       final results = response.data['results'] as List;
       return results.map((movie) => MovieModel.fromJson(movie)).toList();
     } catch (e) {
-      throw Exception('Failed to fetch popular movies: ${e.toString()}');
+      throw UnknownFailure('Failed to fetch popular movies: ${e.toString()}');
+    }
+  }
+
+  Future<MovieDetailsModel> getMovieDetails(String movieID) async {
+    try {
+      final response = await _apiClient.get(Constants.movieDetailsEndpoint,
+          movieID: movieID);
+      return MovieDetailsModel.fromJson(response.data);
+    } catch (e) {
+      throw UnknownFailure('Failed to fetch movie: ${e.toString()}');
     }
   }
 }
