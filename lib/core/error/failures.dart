ac8d205bd265c6b63590bc389c768f253df460bb
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 22:39:49 +0530

    feat: added new failures

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -11,3 +11,15 @@ abstract class Failure extends Equatable {
 class ServerFailure extends Failure {
   const ServerFailure(super.message);
 }
+
+class CacheFailure extends Failure {
+  const CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure(super.message);
+}
+
+class UnknownFailure extends Failure {
+  const UnknownFailure(super.message);
+}
