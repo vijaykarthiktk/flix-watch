96d24b533943327de0b65c532607ff1e08c26b30
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 22:21:50 +0530

    refactor: added new endpoint `movieDetailsEndpoint`

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,4 +4,5 @@ class Constants {
   static const String logoBaseUrl = 'https://image.tmdb.org/t/p/original';
   static const String popularMoviesEndpoint = '/movie/popular';
   static const String movieCreditsEndpoint = '/movie/{movie_id}/credits';
+  static String movieDetailsEndpoint = '/movie/{movie_id}';
 }
