88b1b898a599460e4be56a74d19c027cb97b11ad
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 20:54:22 +0530

    Added Home Screen

---
+++ lib/lib/presentation/views/home/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../coming_soon/coming_soon_screen.dart';
+import '../movies/movie_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flix Watch'),
+      ),
+      body: [const MovieScreen(), const ComingSoonScreen()][_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+          selectedIndex: 0,
+          onDestinationSelected: (index) =>
+              setState(() => _selectedIndex = index),
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.movie_outlined),
+              label: 'Movies',
+              selectedIcon: Icon(Icons.movie),
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.tv_outlined),
+              label: 'TV Shows',
+              selectedIcon: Icon(Icons.tv),
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person_outline),
+              label: 'Peoples',
+              selectedIcon: Icon(Icons.person),
+            ),
+          ]),
+    );
+  }
+}
