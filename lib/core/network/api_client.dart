c10f86f04b9678d6684db2b505be9c2049269068
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 21:47:57 +0530

    feat: added support for movie_id in `ApiClient`

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -4,16 +4,20 @@ import 'package:flixwatch/core/secrets/secrets.dart';
 class ApiClient {
   final Dio _dio;
 
-  ApiClient() : _dio = Dio(BaseOptions(
-    baseUrl: 'https://api.themoviedb.org/3/',
-    headers: {
-      "accept": "application/json",
-      "Authorization": "Bearer ${Secrets.accessTokenAuth}"
-    },
-  ));
-  Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) async {
+  ApiClient()
+      : _dio = Dio(BaseOptions(
+          baseUrl: 'https://api.themoviedb.org/3/',
+          headers: {
+            "accept": "application/json",
+            "Authorization": "Bearer ${Secrets.accessTokenAuth}"
+          },
+        ));
+  Future<Response> get(String path,
+      {Map<String, dynamic>? queryParameters,
+      String movieID = 'movie_id'}) async {
     try {
-      final response = await _dio.get(path, queryParameters: queryParameters);
+      final response = await _dio.get(path.replaceAll('{movie_id}', movieID),
+          queryParameters: queryParameters);
       return response;
     } catch (e) {
       throw Exception(e.toString());
