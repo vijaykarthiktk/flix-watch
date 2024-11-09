8d88e995131e2aa2ee17a99085fb00bf635b195b
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 15:26:55 +0530

    added logoBaseUrl in constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,6 +1,5 @@
 class Constants {
-
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-
+  static const String logoBaseUrl = 'https://image.tmdb.org/t/p/original';
 }
