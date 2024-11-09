cb68dd69a75f64f847e6f8907135ef9ca83c847f
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-09 17:58:38 +0530

    Implemented MovieModel

---
+++ lib/data/models/movie_model.dart
@@ -0,0 +1,59 @@
+import '../../domain/entities/movie.dart';
+
+class MovieModel extends Movie {
+  MovieModel({
+    required super.id,
+    required super.title,
+    required super.overview,
+    required super.posterPath,
+    required super.voteAverage,
+    required super.releaseDate,
+    required super.adult,
+    required super.backdropPath,
+    required super.genreIds,
+    required super.originalLanguage,
+    required super.originalTitle,
+    required super.popularity,
+    required super.video,
+    required super.voteCount,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      adult: json['adult'],
+      backdropPath: json['backdrop_path'],
+      genreIds: List<int>.from(json['genre_ids']),
+      id: json['id'],
+      originalLanguage: json['original_language'],
+      originalTitle: json['original_title'],
+      overview: json['overview'],
+      popularity: (json['popularity'] as num).toDouble(),
+      posterPath: json['poster_path'],
+      releaseDate: json['release_date'],
+      title: json['title'],
+      video: json['video'],
+      voteAverage: (json['vote_average'] as num).toDouble(),
+      voteCount: json['vote_count'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'adult': adult,
+      'backdrop_path': backdropPath,
+      'genre_ids': genreIds,
+      'id': id,
+      'original_language': originalLanguage,
+      'original_title': originalTitle,
+      'overview': overview,
+      'popularity': popularity,
+      'poster_path': posterPath,
+      'release_date': releaseDate,
+      'title': title,
+      'video': video,
+      'vote_average': voteAverage,
+      'vote_count': voteCount,
+    };
+  }
+
+}
