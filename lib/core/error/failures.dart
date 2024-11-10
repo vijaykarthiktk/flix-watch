c5614123d29ce52201137bacb9fd2548a6e8ab82
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2024-11-10 12:46:15 +0530

    feat: added `stackTrace` to `Failure` class

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -2,24 +2,25 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   final String message;
-  const Failure(this.message);
+  final StackTrace? stackTrace;
+  const Failure(this.message, {this.stackTrace});
 
   @override
   List<Object> get props => [message];
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure(super.message);
+  const ServerFailure(super.message, {super.stackTrace});
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure(super.message);
+  const CacheFailure(super.message, {super.stackTrace});
 }
 
 class NetworkFailure extends Failure {
-  const NetworkFailure(super.message);
+  const NetworkFailure(super.message, {super.stackTrace});
 }
 
 class UnknownFailure extends Failure {
-  const UnknownFailure(super.message);
+  const UnknownFailure(super.message, {super.stackTrace});
 }
